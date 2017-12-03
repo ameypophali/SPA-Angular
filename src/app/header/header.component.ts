a32a5d4406dab8030d73f7bd6437170d335cb1a3
ameypophali <amey.pophali@gmail.com>
2017-12-03 17:50:44 -0600

    Header component HTML updated and finalized

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  OnClick(){
+    console.log("clicked");
+    this.router.navigate(["employee"], {relativeTo:this.route});
+  }
 }
