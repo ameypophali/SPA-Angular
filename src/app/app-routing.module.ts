e0edb6c0ff542fcaa162e18395fe4e4e3c287735
ameypophali <amey.pophali@gmail.com>
2017-12-02 21:46:30 -0600

    Initialized app routing ts file

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from  '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+const appRoutes : Routes = [
+    {path: '', redirectTo: '/home', pathMatch:'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about-us', component: AboutUsComponent}
+  ];
+
+@NgModule({
+    imports:[RouterModule.forRoot(appRoutes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule{
+    
+}
