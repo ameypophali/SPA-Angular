3b21671f6e89c18ed21dd4776c038e1b9aefccbd
ameypophali <amey.pophali@gmail.com>
2017-12-04 15:30:25 -0600

    Menu component and children created

---
+++ src/app/menu/menu.model.ts
@@ -0,0 +1,8 @@
+export class MenuItem{
+    constructor(public menuItemId: string,
+                public name: string, 
+                public description: string,
+                public price:number, 
+                public type:string){
+                }
+}
