108b58f20049441005cb98b37ecb09897a8150ac
ameypophali <amey.pophali@gmail.com>
2017-12-04 15:32:07 -0600

    Customer sign up component initialized

---
+++ src/app/customer/customer-signup/customer-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-signup',
+  templateUrl: './customer-signup.component.html',
+  styleUrls: ['./customer-signup.component.css']
+})
+export class CustomerSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
