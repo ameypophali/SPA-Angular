539875334e79b9b095ff276ae4e653d31818512b
ameypophali <amey.pophali@gmail.com>
2017-12-10 17:46:15 -0600

    Updated existing menu components to finish Menu functionality withpout server integration

--- src/app/menu/menu-item/menu-item.component.ts
+++ src/app/menu/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-item',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-item.component.css']
 })
 export class MenuItemComponent implements OnInit {
-
+  @Input() id :number;
+  @Input() name: String;
+  @Input() description: String;
+  @Input() price:number;
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/menu/menu.model.ts
+++ src/app/menu/menu.model.ts
@@ -1,5 +1,5 @@
 export class MenuItem{
-    constructor(public menuItemId: string,
+    constructor(public menuItemId: number,
                 public name: string, 
                 public description: string,
                 public price:number, 
