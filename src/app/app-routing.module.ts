d9372f6706feba5cfdf0c1b99e8142bd84c02618
ameypophali <amey.pophali@gmail.com>
2017-12-11 14:52:28 -0600

    Updated NgModules and app-routing module with child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from  '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
+
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeLoginComponent } from './employee/employee-login/employee-login.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerLoginComponent } from './customer/customer-login/customer-login.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { CustomerSignupComponent } from './customer/customer-signup/customer-signup.component';
+import { MenuItemListComponent } from './menu/menu-item-list/menu-item-list.component';
+import { ItemTypeComponent } from './menu/item-type/item-type.component';
 
 const appRoutes : Routes = [
     {path: '', redirectTo: '/home', pathMatch:'full'},
+    {path: 'contactus', component: ContactUsComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'about-us', component: AboutUsComponent}
+    {path: 'about-us', component: AboutUsComponent},
+    {path: 'menu', component: MenuComponent, children: [
+        {path: '', component: ItemTypeComponent, pathMatch:'full'},
+        {path: ':type', component: MenuItemListComponent}
+    ]},
+    {path: 'employee', component:EmployeeComponent, 
+        children: [
+            {path: '', component: EmployeeLoginComponent,pathMatch:'full'},
+            {path: 'login', component: EmployeeLoginComponent}
+    ]},
+    {path: 'customer', component:CustomerComponent, 
+    children: [
+            {path: '', component: CustomerLoginComponent, pathMatch:'full'},
+            {path: 'login', component: CustomerLoginComponent},
+            {path: 'signup', component: CustomerSignupComponent}
+    ]},
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeLoginComponent } from './employee/employee-login/employee-login.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerLoginComponent } from './customer/customer-login/customer-login.component';
+import { DropdownDirective } from './dropdown.directive';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuDetailsComponent } from './menu-details/menu-details.component';
+import { CustomerSignupComponent } from './customer/customer-signup/customer-signup.component';
+import { ItemTypeListComponent } from './menu/item-type-list/item-type-list.component';
+import { ItemTypeComponent } from './menu/item-type/item-type.component';
+import { MenuItemComponent } from './menu/menu-item/menu-item.component';
+import { MenuItemListComponent } from './menu/menu-item-list/menu-item-list.component';
+import { MenuItemService } from './menu/menu.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutUsComponent,
-    HomeComponent
+    HomeComponent,
+    EmployeeComponent,
+    EmployeeLoginComponent,
+    CustomerComponent,
+    CustomerLoginComponent,
+    DropdownDirective,
+    ContactUsComponent,
+    FooterComponent,
+    MenuComponent,
+    MenuDetailsComponent,
+    CustomerSignupComponent,
+    ItemTypeListComponent,
+    ItemTypeComponent,
+    MenuItemComponent,
+    MenuItemListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [MenuItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
